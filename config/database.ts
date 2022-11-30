98f3bd13f492c0088d6f4a6c5a3ee22666e4aadb
Low_Scarlet <namaetohyaakiradesu@gmail.com>
2022-11-30 23:12:32 +0700

    Init

---
+++ config/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'kandula.db.elephantsql.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'qmknuqgm'),
+      user: env('DATABASE_USERNAME', 'qmknuqgm'),
+      password: env('DATABASE_PASSWORD', 'HYWTqeyZpyC0mvMVvtCIRZ5LVHn5SDsl'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
